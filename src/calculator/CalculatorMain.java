9299ccf8aaf7c8f4002a75c9246c291e29719a36
Danish-dm <danish.mehmood1612@gmail.com>
2018-05-15 15:53:19 +0500

    v1

---
+++ src/calculator/CalculatorMain.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class CalculatorMain {
+
+	public static void main(String[] args) {
+		System.out.println("Ali Hassan G-9");
+
+	}
+
+}
---
+++ src/calculator/TestSuit.java
@@ -0,0 +1,41 @@
+package calculator;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class TestSuit {
+
+	@Test
+	public void testSum() {
+		// Given
+		Calculator calculator = new Calculator();
+		// When
+		int result = calculator.sum(2, 2);
+		// Then
+		if (result != 4) {   // if 2 + 2 != 4
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testMinus() {
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(0, calculator.minus(2, 2));
+	}
+
+	@Test
+	public void testDivide() {
+		Calculator calculator = new Calculator();
+		Assert.assertEquals(2, calculator.divide(6, 3));
+	}
+
+	@Test()
+	public void testDivideWillThrowExceptionWhenDivideOnZero() {
+		Calculator calculator = new Calculator();
+		calculator.divide(6, 1);
+	}
+
+}
